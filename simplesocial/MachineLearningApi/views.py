0887718924b691ebf7cf3e47c59e8d8032c5e24c
bahashmi <bahashmi@rams.colostate.edu>
2019-08-02 14:36:18 +0500

    latestchanges

--- simplesocial/MachineLearningApi/views.py
+++ simplesocial/MachineLearningApi/views.py
@@ -6,11 +6,68 @@ from django.urls import reverse
 from .forms import EventsForm
 from django.core.files.storage import FileSystemStorage
 from django.views.generic import TemplateView
+import pandas as pd
+import numpy as np
 # Create your views here.
 
+def partition(Xparam,Tparam,nfraction,shuffle):
+	nRows = Xparam.shape[0]
+	rows = np.arange(nRows)
+	if shuffle == True:
+		np.random.shuffle(rows)
+
+	nTrain = int(nRows* nfraction )
+	trainRows = rows[:nTrain]
+	testRows = rows[nTrain:]
+	Xtrain, Ttrain = Xparam[trainRows,:], Tparam[trainRows, :]
+	Xtest, Ttest  = Xparam[testRows, :], Tparam[testRows,:]
+	return Xtrain,Ttrain, Xtest, Ttest
+
 def PreprocessPage(request):
-	if "GET" == request.method:
-		return render(request,"preprocess.html")
+	data = pd.read_csv('media/4cols.csv')
+	print(data.shape)
+	data_html = data.to_html()
+	f = open('media/4cols.csv',"r")
+	header = f.readline()
+	print(header)
+	names = header.strip().split(',')
+	names = [i.replace('"','') for i in names]
+	filedata = np.loadtxt(f,delimiter=",")
+	print(names)
+	print(filedata)
+	# df = pd.DataFrame(data=filedata.flatten())
+	# filedataht =pd.DataFrame(data=filedata,index=pd.RangeIndex(range(1, 4)),columns=pd.RangeIndex(range(1, 3)))
+	# filedatahtml = filedataht
+	# filedata_html = filedata.to_html()
+	df = pd.read_csv('media/4cols.csv',skiprows=1)
+	dfcolumnvalues = pd.read_csv('media/4cols.csv')
+	print('dfcolumnvalues[[lights]] ',dfcolumnvalues[['lights']])
+	filedf = dfcolumnvalues[['lights']].to_html
+	# print("df latest ",df.to_html)
+	# print(dfcolumnvalues.loc[:,'lights']) # column values
+	# dfcolumnvalues.loc[:,'lights'].to_html 
+	# print(filedf[:,0:1]) need to learn dataframe indexing
+    #splitting data into test and train 
+	
+	targetvalues = dfcolumnvalues[['lights']].values
+	inputvalues = dfcolumnvalues[['Appliances','T1','RH_1']].values
+	print("T: ", targetvalues[:1])
+	print("X: ",inputvalues[1:])
+	Xtrain,Ttrain, Xtest, Ttest = partition(inputvalues,targetvalues,0.8,True)
+	print("Xtrain: ",Xtrain)
+	dfXtrain = pd.DataFrame(Xtrain) 
+	print("Ttrain: ",Ttrain)
+	dfTtrain = pd.DataFrame(Ttrain)
+	print("Xtest: ",Xtest)
+	dfXtest = pd.DataFrame(Xtest)
+	print("Ttest: ",Ttest)
+	dfTtest = pd.DataFrame(Ttest)
+	return render(request, 'preprocess.html', {'loaded_data': data_html,'filedatahtml':filedf,'Xtrain':dfXtrain.to_html,'Ttrain':dfTtrain.to_html,'Xtest':dfXtest.to_html,'Ttest':dfTtest.to_html})
+	# return render(request,"preprocess.html")
+	# data_html = data.to_html()
+# 	context = {'loaded_data': data_html}
+    # return render(request, 'preprocess.html',{'loaded_data': data_html})
+# return render(request, "dataflow/table.html", context)
 
 def upload_csv(request):
 	data = {}
@@ -21,7 +78,7 @@ def upload_csv(request):
 	csv_file = request.FILES["csv_file"]
 	fs = FileSystemStorage(location='media')
 	filename = fs.save(csv_file.name,csv_file)
-	print(csv_file.name)
+	print("uploaded file in upload_csv ",csv_file.name)
 
 	if not csv_file.name.endswith('.csv'):
 		messages.error(request,'File is not CSV type')
@@ -59,7 +116,7 @@ def upload_csv(request):
 def upload(request):
 	if request.method == 'POST':
 		uploaded_file = request.FILES['myfile']
-		print(uploaded_file.name)
+		print( " the uploaded file ",uploaded_file.name)
 	return render(request,"thanks.html")
 
 
